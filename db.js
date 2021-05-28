9166aa44114e2be811e4a75134ca8bcc3b917126
BlueCarrot-cmd <Leitingdn@t-online.de>
2021-05-28 11:15:22 +0200

    init2

---
+++ db.js
@@ -0,0 +1,13 @@
+{
+    "availableTimeUnits": [
+      {
+        "name": "string",
+        "minValue": 0,
+        "maxValue": 0
+      }
+    ],
+    "selectedTimeUnit": {
+      "timeUnit": "string",
+      "value": 0
+    }
+  }
